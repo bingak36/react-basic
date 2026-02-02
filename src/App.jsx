45c341dca48e37a7b8fdd671ed1510efb5840b38
bingak36 <102848043+bingak36@users.noreply.github.com>
2026-02-02 11:34:54 +0900

    12

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header'
+import Aside from './components/aside'
+import './App.css'
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <Aside />
+      <h2>hello react!</h2>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/aside.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+function Aside() {
+  return (
+    <div className="Aside">
+        aside
+    </div>
+  )
+}
+export default Aside;
